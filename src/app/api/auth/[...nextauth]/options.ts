1edf76629756160d26e5cba32da033a3b80b2bf3
Po-Chun Wu <pcwu2022@gmail.com>
2023-08-21 17:14:12 +0800

    added authentication by credentials

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,30 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: {
+                    label: "Username",
+                    type: "text",
+                    placeholder: "your username"
+                },
+                password: {
+                    label: "Password",
+                    type: "password"
+                }
+            },
+            async authorize(credentials, req) {
+                const user = { id: "0", username: "ddm4534", password: "12345678" };
+                if (credentials?.username === user.username && credentials?.password === user.password){
+                    return user;
+                } else {
+                    return null;
+                }
+            },
+        })
+    ]
+}
