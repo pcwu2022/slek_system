37e8b4497381abebbbbd227f0d31e437fea21dbb
Po-Chun Wu <pcwu2022@gmail.com>
2023-08-26 11:00:10 +0800

    created folder ram_db

--- src/app/projects/hyponatremia/page.tsx
+++ src/app/projects/hyponatremia/page.tsx
@@ -11,11 +11,7 @@ const data: PageData = {
 const hyponatremia = () => {
   return (
     <div>
-      <iframe 
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" 
-        frameBorder="0"
-        className='w-screen min-h-screen no-scrollbar -md-4'
-      ></iframe>
+      
     </div>
   )
 }
