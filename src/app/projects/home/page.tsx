e422893e23ccc7c773f75ccefd54cc78786bdea6
Po-Chun Wu <pcwu2022@gmail.com>
2023-08-31 21:05:59 +0800

    Version One of SLEK System!

--- src/app/projects/home/page.tsx
+++ src/app/projects/home/page.tsx
@@ -4,7 +4,7 @@ import { PageData } from '../types/types';
 
 const pageData: Array<PageData> = [
   {
-    title: "Hyponatremia",
+    title: "Hyponatremia", 
     name: "hyponatremia",
     description: "Hyponatremia is low blood sodium concentration, causing fluid imbalance, nausea, confusion, and potentially dangerous neurological complications.",
     imageLink: "https://www.verywellhealth.com/thmb/6bpKlPELKuI6lylP9lULkDehH6I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/hyponatremia-overview-4586684_final-fc297d28a05a401c967ff6cc8a64d79d.png"
