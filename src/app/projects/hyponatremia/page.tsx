f2c18c7eb609a4c6ba4689990756e34d4e436eee
Po-Chun Wu <pcwu2022@gmail.com>
2023-08-31 21:03:57 +0800

    export of pages fixed

--- src/app/projects/hyponatremia/page.tsx
+++ src/app/projects/hyponatremia/page.tsx
@@ -1,12 +1,4 @@
 import React from 'react';
-import type { PageData } from '../types/types';
-
-const data: PageData = {
-  title: "Hyponatremia",
-  name: "hyponatremia",
-  description: "Hyponatremia is low blood sodium concentration, causing fluid imbalance, nausea, confusion, and potentially dangerous neurological complications.",
-  imageLink: "https://www.verywellhealth.com/thmb/6bpKlPELKuI6lylP9lULkDehH6I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/hyponatremia-overview-4586684_final-fc297d28a05a401c967ff6cc8a64d79d.png"
-};
 
 const hyponatremia = () => {
   return (
@@ -21,4 +13,3 @@ const hyponatremia = () => {
 }
 
 export default hyponatremia;
-export { data };
--- src/app/projects/shock/page.tsx
+++ src/app/projects/shock/page.tsx
@@ -1,12 +1,4 @@
 import React from 'react';
-import { PageData } from '../types/types';
-
-const data: PageData = {
-  title: "Shock",
-  name: "shock",
-  description: "Shock is a life-threatening condition where insufficient blood flow impairs vital organs, causing low blood pressure, rapid heartbeat, and confusion.",
-  imageLink: "https://www.webpagescreenshot.info/image-url/bLDZaHuBj"
-};
 
 const shock = () => {
   return (
@@ -21,4 +13,3 @@ const shock = () => {
 }
 
 export default shock;
-export { data };
