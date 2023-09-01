8c9952fffa41d074dd3d4751da1a03c23e76b91b
Po-Chun Wu <pcwu2022@gmail.com>
2023-09-01 10:39:24 +0800

    added levels.png

--- src/app/projects/hyponatremia/[id]/components/PE.tsx
+++ src/app/projects/hyponatremia/[id]/components/PE.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { SheetJson } from '../../ram_db/types';
 import * as enums from '../../ram_db/enums';
 
+// components
+import { StyledTitle, StyledBox } from './StyledComponents';
+
 const PE = (props: {data: SheetJson}) => {
   return (
     <div>
