ac15b7bc08c1d659d4b29fa0fdc71e9ee9acb24c
Po-Chun Wu <pcwu2022@gmail.com>
2023-09-28 21:45:53 +0800

    https problem

--- src/app/projects/hypoxia/page.tsx
+++ src/app/projects/hypoxia/page.tsx
@@ -4,7 +4,7 @@ const hypoxia = () => {
   return (
     <div>
       <iframe 
-        src="http://cindy12347.pythonanywhere.com/" 
+        src="https://cindy12347.pythonanywhere.com/" 
         frameBorder="0"
         className='w-screen min-h-screen no-scrollbar -md-4'
       ></iframe>
