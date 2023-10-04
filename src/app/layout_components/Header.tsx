1e110cbe17834f945aaad0d36be1a8f9a4c65e0a
Po-Chun Wu <pcwu2022@gmail.com>
2023-10-04 19:32:00 +0800

    fixed login/logout system

--- src/app/layout_components/Header.tsx
+++ src/app/layout_components/Header.tsx
@@ -6,8 +6,11 @@ import Link from 'next/link';
 
 import ProfileImage from './ProfileImage';
 import HeaderDropdown from './HeaderDropdown';
+import { SessionProvider} from 'next-auth/react';
 
-const Header = () => {
+const Header = (props: {
+  loggedIn?: boolean
+}) => {
   const [hover, setHover] = useState<boolean>(true);
   const [stickerHover, setStickerHover] = useState<boolean>(false);
 
@@ -17,53 +20,54 @@ const Header = () => {
     }, 1000);
   }, []);
   return (
-    <div
-      className='z-40 relative'
-      onMouseEnter={(e) => {
-        setHover(true);
-      }}
-      onMouseLeave={(e) => {
-        setHover(false);
-      }}
-    >
-      <div className={"bg-blue-950 w-full h-20 duration-500 " + (hover?"mt-0":"-mt-20")}>
-        <Link href="../../">
-          <Image 
-            src = "/slek_image.webp"
-            alt = "slek image"
-            width = {65}
-            height = {43}
-            className="absolute mt-3 ml-12"
+    <SessionProvider>
+      <div
+        className='z-40 relative'
+        onMouseEnter={(e) => {
+          setHover(true);
+        }}
+        onMouseLeave={(e) => {
+          setHover(false);
+        }}
+      >
+        <div className={"bg-blue-950 w-full h-20 duration-500 " + (hover?"mt-0":"-mt-20")}>
+          <Link href="../../">
+            <Image 
+              src = "/slek_image.webp"
+              alt = "slek image"
+              width = {65}
+              height = {43}
+              className="absolute mt-3 ml-12"
+            />
+            <Image 
+              src = "/slek_logo.webp"
+              alt = "slek logo"
+              width = {141}
+              height = {32}
+              className="absolute mt-5 ml-20"
+            />
+          </Link>
+          <ProfileImage 
+            imageLink='https://cdn-icons-png.flaticon.com/512/149/149071.png'
+            onMouseOver={(e) => {
+              setStickerHover(true);
+            }}
+            onMouseLeave={(e) => {
+              setTimeout(() => {
+                setStickerHover(false);
+              }, 1000);
+            }}
           />
-          <Image 
-            src = "/slek_logo.webp"
-            alt = "slek logo"
-            width = {141}
-            height = {32}
-            className="absolute mt-5 ml-20"
+          
+        </div>
+        {
+          <HeaderDropdown 
+            display={stickerHover}
           />
-        </Link>
-        <ProfileImage 
-          imageLink='https://cdn-icons-png.flaticon.com/512/149/149071.png'
-          onMouseOver={(e) => {
-            setStickerHover(true);
-          }}
-          onMouseLeave={(e) => {
-            setTimeout(() => {
-              setStickerHover(false);
-            }, 1000);
-          }}
-        />
-        
+        }
+        <div className='h-10 relative -mb-10'></div>
       </div>
-      {
-        <HeaderDropdown 
-          display={stickerHover}
-          loggedIn={true}
-        />
-      }
-      <div className='h-10 relative -mb-10'></div>
-    </div>
+    </SessionProvider>
   )
 }
 
