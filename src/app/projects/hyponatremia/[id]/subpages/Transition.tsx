6f5b7a55e556661318f6593803c8fbf8f2bd7d50
Po-Chun Wu <pcwu2022@gmail.com>
2023-10-04 19:49:07 +0800

    added transition

---
+++ src/app/projects/hyponatremia/[id]/subpages/Transition.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SheetJson } from '../../ram_db/types';
+
+const Transition = (props: {
+  data: SheetJson
+}) => {
+  return (
+    <div className='w-full h-full'>
+      
+    </div>
+  )
+}
+
+export default Transition;
