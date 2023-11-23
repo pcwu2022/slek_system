4ce1e20eba72742e233fbbf709954ab0ecb4960b
Po-Chun Wu <pcwu2022@gmail.com>
2023-11-23 19:44:49 +0800

    Fixed Control Flow

--- src/app/projects/hyponatremia/[id]/subpages/ClientPage.tsx
+++ src/app/projects/hyponatremia/[id]/subpages/ClientPage.tsx
@@ -50,16 +50,16 @@ const ClientPage = ( props: { db: DBJson } ) => {
           (state === enums.State.CC) ? <CC data={sheet} /> : 
           (state === enums.State.MedHistory) ? <MedHistory data={sheet} /> : 
           (state === enums.State.PE) ? <PE data={sheet} inquiryHistory={inquiryHistory} setInquiryHistory={setInquiryHistory} /> : 
-          (state === enums.State.Diagnosis1) ? <Diagnosis data={sheet} diagnosisHistory={diagnosisHistory} setDiagnosisHistory={setDiagnosisHistory} dayCounter={dayCounter} /> : 
+          (state === enums.State.Diagnosis1) ? <Diagnosis final={false} data={sheet} diagnosisHistory={diagnosisHistory} setDiagnosisHistory={setDiagnosisHistory} dayCounter={dayCounter} /> : 
           (state === enums.State.Examination) ? <Examination data={sheet} /> : 
-          (state === enums.State.Diagnosis2) ? <Diagnosis data={sheet} diagnosisHistory={diagnosisHistory} setDiagnosisHistory={setDiagnosisHistory} dayCounter={dayCounter} /> : 
+          (state === enums.State.Diagnosis2) ? <Diagnosis final={false} data={sheet} diagnosisHistory={diagnosisHistory} setDiagnosisHistory={setDiagnosisHistory} dayCounter={dayCounter} /> : 
           (state === enums.State.Therapy) ? <Therapy data={sheet} dayCounter={dayCounter} therapyHistory={therapyHistory} setTherapyHistory={setTherapyHistory} /> : 
-          (state === enums.State.DiagnosisF) ? <Diagnosis data={sheet} diagnosisHistory={diagnosisHistory} setDiagnosisHistory={setDiagnosisHistory} dayCounter={-1} /> : 
+          (state === enums.State.DiagnosisF) ? <Diagnosis final={true} data={sheet} diagnosisHistory={diagnosisHistory} setDiagnosisHistory={setDiagnosisHistory} dayCounter={dayCounter} /> : 
           (state === enums.State.Transition) ? <Transition data={sheet} dayCounter={dayCounter} state={state} setState={setState} /> : 
           (state === enums.State.Success) ? <Success data={sheet} /> : 
           (state === enums.State.ExaminationN) ? <Examination data={sheet} /> : 
           (state === enums.State.TherapyN) ? <Therapy data={sheet} dayCounter={dayCounter} therapyHistory={therapyHistory} setTherapyHistory={setTherapyHistory} /> : 
-          (state === enums.State.DiagnosisN) ? <Diagnosis data={sheet} diagnosisHistory={diagnosisHistory} setDiagnosisHistory={setDiagnosisHistory} dayCounter={dayCounter} /> : 
+          (state === enums.State.DiagnosisN) ? <Diagnosis final={false} data={sheet} diagnosisHistory={diagnosisHistory} setDiagnosisHistory={setDiagnosisHistory} dayCounter={dayCounter} /> : 
           (state === enums.State.Fail) ? <Fail data={sheet} /> : <></>
         }
       </div>
